a622a8ed7b5b6dc03a3ecd917b884496547928f3
DawrAli33285 <lemightyeagle@gmail.com>
2025-05-19 21:54:41 +0500

    new

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express=require('express')
 const cors=require('cors')
+const mongoose=require('mongoose')
 
 const app=express();
 app.use(cors())
+
+mongoose.connect(`mongodb://127.0.0.1/csvfileupload`)
 
